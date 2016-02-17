530a000bf51327a3394fd461a1619bed56e7d0db
Carlos Núñez Aguilera <carlosnaguilera@gmail.com>
2016-02-17 01:15:06 +0100

    Initial Commit

---
+++ Wallamarvel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wallamarvel
+//
+//  Created by Carlos Núñez Aguilera on 17/2/16.
+//  Copyright © 2016 Qualoo Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WallamarvelTests/WallamarvelTests.swift
@@ -0,0 +1,36 @@
+//
+//  WallamarvelTests.swift
+//  WallamarvelTests
+//
+//  Created by Carlos Núñez Aguilera on 17/2/16.
+//  Copyright © 2016 Qualoo Apps. All rights reserved.
+//
+
+import XCTest
+@testable import Wallamarvel
+
+class WallamarvelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
